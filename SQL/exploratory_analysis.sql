970f3a9625d39bfa4771a1ec4c5a059185b3bc8a
Kevin Orellana <kevinorellana9@gmail.com>
2026-06-01 10:36:11 -0400

    Update exploratory_analysis.sql

--- SQL/exploratory_analysis.sql
+++ SQL/exploratory_analysis.sql
@@ -1,1 +1,44 @@
 
+/*Used subqueries to calculate the month-over-month change in citywide arrests.*/
+SELECT
+			(SELECT
+					count(*)
+			FROM
+					nypd_arrests
+			WHERE
+					arrest_date BETWEEN '2026-02-01' AND '2026-02-28') -  	--finds the feb arrests
+			(SELECT
+					count(*)
+			FROM
+					nypd_arrests
+			WHERE
+					arrest_date BETWEEN '2026-01-01' AND '2026-01-31') AS 'jan_to_feb_change', 
+			(SELECT
+					count(*)
+			FROM
+					nypd_arrests
+			WHERE
+					arrest_date BETWEEN '2026-03-01' AND '2026-03-31') - 		--finds march arrest
+			(SELECT
+					count(*)
+			FROM
+					nypd_arrests
+			WHERE
+					arrest_date BETWEEN '2026-02-01' AND '2026-02-28')  AS 'feb_to_march_change';
+
+/*This query examines which misdemeanor charges occur most often throughout the dataset.*/
+
+SELECT
+	police_description,
+	lvl_of_offense,
+	COUNT(*) AS 'amount_reported'
+FROM 
+	nypd_arrests
+WHERE 
+	lvl_of_offense = 'Misdemeanor' 
+GROUP BY 
+	police_description
+ORDER BY
+	amount_reported DESC
+LIMIT 10;
+
